9da1d9bf76e4b772a4ef61f65128d22172895a1c
Erik Qvarnström <qvarnstrom.erik@gmail.com>
2017-11-03 15:46:28 +0100

    Day 1 Start of shit scene
    some shit scene assets that will be deleted. Character controllers and carera behaviour.

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    IInteractable currentInteractable;
+
+    CharacterController cc;
+
+    [SerializeField]
+    float moveSpeed, rotSpeed;
+
+    float vertical, horizontal;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Move();
+        if (Input.GetKeyDown(KeyCode.E) && currentInteractable != null)
+        {
+            currentInteractable.DoAction();
+        }
+    }
+
+    void Move()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal") * Time.deltaTime * rotSpeed;
+        transform.Rotate(0, horizontal, 0, Space.Self);
+        vertical = Input.GetAxisRaw("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(0, 0, vertical, Space.Self);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        IInteractable otherInterface = other.gameObject.GetComponent<IInteractable>();
+        if (otherInterface != null)
+        {
+            otherInterface.ShowAction(true);
+            currentInteractable = otherInterface;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        IInteractable otherInterface = other.gameObject.GetComponent<IInteractable>();
+        if (otherInterface != null)
+        {
+            otherInterface.ShowAction(false);
+            currentInteractable = null;
+        }
+    }
+}
