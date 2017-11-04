e150e7caf307390e2f3d67a2605c020ea3651f4d
BoogeyMoon <qvarnstrom.erik@gmail.com>
2017-11-04 13:29:51 +0100

    New character to controll
    -Deleted old fbx and prefab.
    
    -Added new fbx and prefab
    
    -Connected camera to new.

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -12,7 +12,7 @@ public class CameraBehaviour : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        Player = GameObject.Find("Char_TestMain_PF");
+        Player = GameObject.Find("Template_Female_Mesh_3");
         PlayerScript = Player.GetComponent<PlayerControls>();
         x = transform.position.x - Player.transform.position.x;
         y = transform.position.y - Player.transform.position.y;
