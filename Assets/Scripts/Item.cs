179aa682e1c2d7f1180e616640fc4987602c16bb
Stinan <Stinan.H@gmail.com>
2017-11-06 13:47:18 +0100

    cleaning up branch yo bro
    fixat dåra

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item {
+
+    public int itemID;
+    public string itemName;
+    public string itemDesc;
+    public Sprite itemIcon;
+    public GameObject go;
+
+}
