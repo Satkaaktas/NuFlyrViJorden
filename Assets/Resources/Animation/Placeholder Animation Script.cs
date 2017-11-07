3a94ef840a36e88ea273f9c2f0c09c741b493ca9
BoogeyMoon <qvarnstrom.erik@gmail.com>
2017-11-07 22:58:56 +0100

    Memes and Dreamsss
    Animation and Avatat the Second game

---
+++ Assets/Resources/Animation/Placeholder Animation Script.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceholderAnimationScript : MonoBehaviour {
+
+    Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = this.GetComponent<Animator>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //animator.SetFloat(5, 1);
+
+    }
+}
