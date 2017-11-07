e76313f70e14148f9d9bc88a3fe7de693131de42
KazzTruul <kazztruul@hotmail.com>
2017-11-07 11:34:09 +0100

    MiniMap v.1
    minimap fixad (typ)

---
+++ Assets/Resources/Scripts/MiniMapCamScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapCamScript : MonoBehaviour
+{
+    void Start()
+    {
+        transform.Rotate(transform.rotation.x, -27f, transform.rotation.z, Space.World);
+    }
+}
