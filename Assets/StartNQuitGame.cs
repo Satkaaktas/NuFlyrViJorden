d644777b8fe789679589bd719c176840f6d50ac9
Afro663 <afro663@hotmail.com>
2017-11-07 12:20:44 +0100

    Huvudmeny + Credits - Graphics
    Inclu. unitypackage

---
+++ Assets/StartNQuitGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartNQuitGame : MonoBehaviour {
+
+    public void startNew() //Used to load a new game.
+    {
+        SceneManager.LoadScene ("XXX", LoadSceneMode.Single); //TODO Change XXX to scene of game.
+    }
+
+    //TODO Add load function here.
+
+    public void quit() //Used to quit the game.
+    {
+        Application.Quit(); //Doesn't work when debugging in unity.
+    }
+}
