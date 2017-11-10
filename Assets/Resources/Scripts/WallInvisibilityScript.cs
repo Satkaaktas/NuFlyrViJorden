306a39eb020b4e3b4257eaf13db98dcb9ce4d394
Satkaaktas <Timmy_nitr0@hotmail.com>
2017-11-10 16:20:32 +0100

    Halvfärdig Wallclipper

--- Assets/Resources/Scripts/WallInvisibilityScript.cs
+++ Assets/Resources/Scripts/WallInvisibilityScript.cs
@@ -3,46 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class WallInvisibilityScript : MonoBehaviour {
+public class WallInvisibilityScript : MonoBehaviour
+{
 
     public Transform _camera;
 
-    private List<Renderer> renderers = new List<Renderer>();
-    	
-	void Update () {
+    List<Material> mats;
+
+    void Start()
+    {
+        mats = new List<Material>();
+    }
+
+    void Update()
+    {
         Ray ray = new Ray(transform.position, _camera.position - transform.position);
-
         RaycastHit[] hits;
-
         hits = Physics.RaycastAll(ray);
 
-        Renderer[] renders = new Renderer[hits.Length];
-
-        for(int i = 0; i < hits.Length; i++)
+        for (int i = 0; i < hits.Length; i++)
         {
             RaycastHit hit = hits[i];
-            Renderer rend = hit.transform.GetComponent<Renderer>();
-
-            if (rend)
+            if (hit.transform.gameObject.tag == "Wall")
             {
-                rend.enabled = false;
-                renders[i] = rend;
-
-                if (!renderers.Contains(rend))
+                GameObject wall = hit.transform.gameObject;
+                Material mat = wall.GetComponent<Renderer>().material;
+                for (int j = 0; j < mats.Count; j++)
                 {
-                    renderers.Add(rend);
+                    if (mats[0] == mat)
+                    {
+                        
+                    }
                 }
+                Color invisColor = wall.GetComponent<Renderer>().material.color;
+                invisColor.a = 0.2f;
+                mats.Add(wall.GetComponent<Renderer>().material);
+                mats[mats.Count - 1].color = invisColor;
             }
 
         }
-
-        for (int i = 0; i < renderers.Count; i++)
-        {
-            if(!renders.Contains(renderers[i]))
-            {
-                renderers[i].transform.GetComponent<Renderer>().enabled = true;
-                renderers.Remove(renderers[i]);
-            }
-        }
-	}
+    }
 }
