89514ab129befe0f9582716fa502256e3646c35c
Satkaaktas <Timmy_nitr0@hotmail.com>
2017-11-10 23:36:54 +0100

    BuggigtWallHack
    Får förfina scriptet lite senare, men det fungerar iaf just nu

--- Assets/Resources/Scripts/WallInvisibilityScript.cs
+++ Assets/Resources/Scripts/WallInvisibilityScript.cs
@@ -7,8 +7,8 @@ public class WallInvisibilityScript : MonoBehaviour
 {
 
     public Transform _camera;
-
     List<Material> mats;
+    bool flag;
 
     void Start()
     {
@@ -20,9 +20,11 @@ public class WallInvisibilityScript : MonoBehaviour
         Ray ray = new Ray(transform.position, _camera.position - transform.position);
         RaycastHit[] hits;
         hits = Physics.RaycastAll(ray);
+        List<Material> dontChangeBack = new List<Material>();
 
         for (int i = 0; i < hits.Length; i++)
         {
+            flag = false;
             RaycastHit hit = hits[i];
             if (hit.transform.gameObject.tag == "Wall")
             {
@@ -30,17 +32,41 @@ public class WallInvisibilityScript : MonoBehaviour
                 Material mat = wall.GetComponent<Renderer>().material;
                 for (int j = 0; j < mats.Count; j++)
                 {
-                    if (mats[0] == mat)
+                    if (mats[j] == mat)
                     {
-                        
+                        dontChangeBack.Add(mat);
+                        flag = true;
                     }
                 }
-                Color invisColor = wall.GetComponent<Renderer>().material.color;
-                invisColor.a = 0.2f;
-                mats.Add(wall.GetComponent<Renderer>().material);
-                mats[mats.Count - 1].color = invisColor;
+                if (!flag)
+                {
+                    Color invisColor = wall.GetComponent<Renderer>().material.color;
+                    invisColor.a = 0.2f;
+                    mats.Add(wall.GetComponent<Renderer>().material);
+                    mats[mats.Count - 1].color = invisColor;
+                }
             }
-
+        }
+        if (mats.Count != dontChangeBack.Count)
+            RestoreMaterials(dontChangeBack);
+    }
+    void RestoreMaterials(List<Material> dCB)
+    {
+        for (int i = 0; i < mats.Count; i++)
+        {
+            for (int j = 0; j < dCB.Count; j++)
+            {
+                if (mats[i] == dCB[j])
+                    break;
+                if (j == dCB.Count - 1)
+                {
+                    Color originalColor = mats[i].color;
+                    originalColor.a = 1.0f;
+                    mats[i].color = originalColor;
+                    mats.RemoveAt(i);
+                    i--;
+                }
+            }
         }
     }
 }
