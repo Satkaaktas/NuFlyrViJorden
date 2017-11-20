fdf20a177b2b75a236a3a22a3c3519bcb962a4e9
KazzTruul <kazztruul@hotmail.com>
2017-11-20 11:01:31 +0100

    mer pausefix

--- Assets/Resources/Scripts/PauseGame.cs
+++ Assets/Resources/Scripts/PauseGame.cs
@@ -13,6 +13,7 @@ public class PauseGame : MonoBehaviour
 
     public Transform canvas;
 
+
     PlayerControls player;
 
     void Start()
@@ -37,14 +38,15 @@ public class PauseGame : MonoBehaviour
             player.InputEnabled = false;
             canvas.gameObject.SetActive(true); // I sånnafall, gör den true så man ser paus menyn och samt
             Time.timeScale = 0;                // så sätter vi skalan där tiden passerar till 0 så allt stannar i bakgrunden
+            AudioListener.volume = 0.5f;        // Musik eller annan audio som spelas i bakgrunden sänks i paus menyn.
         }
         else
         {
             player.InputEnabled = true;
             canvas.gameObject.SetActive(false); // När vi återgår till spelet som kan ske via att man trycker ESC igen eller Resume knappen
             Time.timeScale = 1;                 // så återgår vi till spelet, canvas blir false igen och tids skalan återgår till normalt.
+            AudioListener.volume = 1;
         }
-
 
     }
 }
