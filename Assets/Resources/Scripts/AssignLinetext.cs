d1f15da35bb8b6d1d96af0ec6b2f4fbfe2282581
RemiRed <andrelito96@hotmail.com>
2017-11-20 14:14:14 +0100

    -

---
+++ Assets/Resources/Scripts/AssignLinetext.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AssignLinetext : MonoBehaviour {               /*By Björn Andersson*/
+
+    bool assigned = false;
+
+    void Awake()
+    {
+        if (!assigned)
+        {
+            GameObject.Find("DialogueManager").GetComponent<TextColorChanger>().LineText = GetComponent<Text>();
+        }
+    }
+}
